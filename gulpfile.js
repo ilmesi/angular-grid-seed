c76b61c4e15fc6e79964d620fd8fd340b66f34a5
ilmesi <nachoguason@gmail.com>
2016-03-10 20:31:43 +0100

    gulpfile working

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,39 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var browserSync = require('browser-sync');
 var wiredep = require('wiredep').stream;
-var reload = browserSync.reload;
 var $ = require('gulp-load-plugins')({lazy: true});
+var reload = browserSync.reload;
 
 
 function inject(src, label, order) {
-    var options = {read: false};
-    if (label) {
-        options.name = 'inject:' + label;
-    }
-    return $.inject(orderSrc(src, order), options);
+  var options = {
+    read: false,
+    relative: true
+  };
+  if (label) {
+    options.name = 'inject:' + label;
+  }
+  return $.inject(orderSrc(src, order), options);
 }
 function orderSrc (src, order) {
-    return gulp
-        .src(src)
-        .pipe($.if(order, $.order(order)));
+  return gulp
+    .src(src)
+    .pipe($.if(order, $.order(order)));
 }
+
+gulp.task('inject', ['wiredep'], function() {
+  return gulp
+    .src('./client/index.html')
+    .pipe(inject('./client/css/styles.css'))
+    .pipe(gulp.dest('./client/'));
+});
 
 // Inject bower dependencies & project js into index.html
 gulp.task('wiredep', function () {
   var options = {
     bowerJson: require('./bower.json'),
     directory: './bower_components/',
-    ignorePath: '../..'
+    ignorePath: '../'
   };
   var jsOrder = [
     '**/app.module.js',
@@ -41,23 +51,29 @@ gulp.task('wiredep', function () {
     .src('client/index.html')
     .pipe(wiredep(options))
     .pipe(inject(js, '', jsOrder))
-    .pipe(gulp.dest('./app'));
+    .pipe(gulp.dest('./client'));
 });
 
 gulp.task('sass', function() {
   return sass('client/scss/styles.scss')
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('client/css'))
     .pipe(reload({ stream:true }));
 });
 
 gulp.task('browserSyncReload', reload);
 
-gulp.task('serve', ['sass'], function() {
-    browserSync({
-        server: {
-          baseDir: './client',
-        }
-    });
+gulp.task('serve-dev', ['inject'], function() {
+  browserSync({
+    server: {
+      baseDir: 'client',
+      routes: {
+          "/bower_components": "bower_components"
+      }
+    },
+    files: [
+      './client/**/*.*'
+    ]
+  });
 
-    gulp.watch(['client/scss/*.scss', 'client/**/*.html', 'client/**/*.js'], ['sass', 'browserSyncReload']);
+  gulp.watch(['client/scss/*.scss', 'client/**/*.html', 'client/**/*.js'], ['sass', 'browserSyncReload']);
 });
