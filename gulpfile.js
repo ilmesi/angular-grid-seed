610c55078601b34f0000e340d0aad374e385a64b
ilmesi <nachoguason@gmail.com>
2016-03-10 16:02:52 +0100

    wip gulp wiredep

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,48 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var browserSync = require('browser-sync');
+var wiredep = require('wiredep').stream;
 var reload = browserSync.reload;
+var $ = require('gulp-load-plugins')({lazy: true});
 
+
+function inject(src, label, order) {
+    var options = {read: false};
+    if (label) {
+        options.name = 'inject:' + label;
+    }
+    return $.inject(orderSrc(src, order), options);
+}
+function orderSrc (src, order) {
+    return gulp
+        .src(src)
+        .pipe($.if(order, $.order(order)));
+}
+
+// Inject bower dependencies & project js into index.html
+gulp.task('wiredep', function () {
+  var options = {
+    bowerJson: require('./bower.json'),
+    directory: './bower_components/',
+    ignorePath: '../..'
+  };
+  var jsOrder = [
+    '**/app.module.js',
+    '**/*.module.js',
+    '**/*.js'
+  ]
+  var js = [
+    './app/**/*.module.js',
+    './app/**/*.js',
+    '!./app/**/*.spec.js'
+  ]
+
+  return gulp
+    .src('app/index.html')
+    .pipe(wiredep(options))
+    .pipe(inject(js, '', jsOrder))
+    .pipe(gulp.dest('./app'));
+});
 
 gulp.task('sass', function() {
   return sass('app/scss/styles.scss')
@@ -15,10 +55,7 @@ gulp.task('browserSyncReload', reload);
 gulp.task('serve', ['sass'], function() {
     browserSync({
         server: {
-          baseDir: 'app',
-          routes: {
-            "/bower_components": "bower_components"
-          }
+          baseDir: './',
         }
     });
 
